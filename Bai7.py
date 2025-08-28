27e4aabc7597c574b45a346393054bd1147c7935
DTH235644-LENHUTHAO <lenhuthao4812@gmail.com>
2025-08-28 08:52:19 +0700

    BaiTapChuong1

---
+++ Bai7.py
@@ -0,0 +1,3 @@
+# Nhập chuỗi
+chuoi=input("Nhập một chuỗi từ bàn phím:")
+print("Chuỗi vừa nhập là:", chuoi)
---
+++ Bai9.py
@@ -0,0 +1,4 @@
+d=int(input("Nhap chieu dai:"))
+rong=int(input("Nhap chieu rong:"))
+for i in range(rong):
+    print("* "*d)
