b6260a97ede935200f0882312c3b6ccda5845518
DTH235644-LENHUTHAO <lenhuthao48122gmail.com>
2025-09-01 18:31:24 +0700

    Them1/9

---
+++ BaiTapChuong2/Cau3.py
@@ -0,0 +1,7 @@
+print("Chuong trinh tinh diem:")
+toan=float(input("Nhap diem Toan:"))
+ly=float(input("Nhap diem Ly:"))
+hoa=float(input("Nhap dien Hoa:"))
+dtb=(toan+ly+hoa)/3
+print("Diem trung binh:",dtb)
+print("Diem trung binh:",round(dtb,2))
