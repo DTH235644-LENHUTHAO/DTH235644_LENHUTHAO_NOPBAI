41d7293f027b35bb5d96728a5de5502e5bf0b285
DTH235644-LENHUTHAO <lenhuthao48122gmail.com>
2025-09-18 20:13:56 +0700

    add

---
+++ BaiTapChuong4/Cau12.py
@@ -0,0 +1,5 @@
+def oscillate():
+    for n in range(-3, 5):
+        print(n, -n, end=' ')
+
+oscillate()
---
+++ BaiTapChuong4/Cau6.py
@@ -0,0 +1,6 @@
+import random
+
+def random_in_range():
+    #Trả về một số nguyên ngẫu nhiên từ 0 đến 99
+    return random.randrange(0, 100)
+print(random_in_range())
